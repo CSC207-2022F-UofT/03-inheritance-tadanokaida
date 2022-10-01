47d29340ebe491d54b58bc0eb075830418dbf885
tadanokaida <kevinkhuang2002@gmail.com>
2022-10-01 04:25:50 -0400

    draft 2

--- src/main/java/CrossbodyBag.java
+++ src/main/java/CrossbodyBag.java
@@ -49,7 +49,7 @@ public class CrossbodyBag extends Bag{
 
     @Override
     public String toString() {
-        return this.getColor() + "Crossbody Bag with {" + numberOfStraps+ "} (" + this.getNumberOfContents() + " / " +
-                this.getCapacity() + ")";
+        return this.getColor() + "Crossbody Bag with {" + numberOfStraps+ "} straps (" + this.getNumberOfContents()
+                + " / " + this.getCapacity() + ")";
     }
 }
